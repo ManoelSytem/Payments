e6d7b3de3ece870e4fbe512ec0f49aaf7e5258bb
Manoel Neto <manoel.n@lyncas.net>
2022-08-31 18:54:38 -0300

    feat : modulo pagamento - trabalhando na camada repostiory

---
+++ WebAPI/WebAPI/Repository/PagamentoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace WebAPI.Repository
+{
+    public class PagamentoRepository : IPagamento
+    {
+        public Task<object> ObterPagamentoId()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
