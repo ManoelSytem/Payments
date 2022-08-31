3ff48621616b5b58d088e505d0c81942c9858c15
Manoel Neto <manoel.n@lyncas.net>
2022-08-31 19:23:15 -0300

    feat: modulo pagamento - trabalhando comada repository

--- WebAPI/WebAPI/Repository/PagamentoRepository.cs
+++ WebAPI/WebAPI/Repository/PagamentoRepository.cs
@@ -1,12 +1,23 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace WebAPI.Repository
 {
     public class PagamentoRepository : IPagamento
     {
-        public Task<object> ObterPagamentoId()
+        public Task<object> DeletePorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> ObterPagamentoId(Guid id)
         {
             throw new System.NotImplementedException();
+        }
+
+        public Task<object> ObterPorId(Guid id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
